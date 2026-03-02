bd9ac6bc4ae8092407c66dbc216319f426ccb2ea
viniciusegydio <egydiovinicius022@gmail.com>
2026-03-02 15:25:23 -0300

    initial commit

---
+++ ado1/neander.h
@@ -0,0 +1,23 @@
+#ifndef NEANDER_H
+#define NEANDER_H
+#define TAM_MEM 256 //Padrão do Neander
+
+#include <stdint.h> //Necessário para utilizarmos variáveis com 8 bits
+
+typedef{
+    //Memória:
+    uint8_t memoria[TAM_MEM]; 
+    uint8_t memoriaOrig[TAM_MEM]; 
+
+    //AC e PC:
+    uint8_t acc;
+    uint8_t pc;
+
+    //Flags:
+    uint8_t flagNegativa;
+    uint8_t flagZero;
+
+    //Acessos e instruções:
+    uint32_t acessosMem;
+    uint32_t instruçõesExecutadas;
+}neander;
