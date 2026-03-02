c8d7f2200c842cee0d6681800617f20c4e4208fe
viniciusegydio <egydiovinicius022@gmail.com>
2026-03-02 15:43:33 -0300

    finishes implementation of functions

--- ado1/neander.h
+++ ado1/neander.h
@@ -21,3 +21,12 @@ typedef{
     uint32_t acessosMem;
     uint32_t instruçõesExecutadas;
 }neander;
+
+//Definição das funções:
+void iniciarNeander(neander *arg);
+void executarPrograma(neander *programa);
+void printEstado(neander *arg, int modo); //"modo" deve ser hexa ou dec
+void printMemoria(uint8_t *mem, int modo);
+int carregarPrograma(neander *programa, const char *file);
+
+#endif
